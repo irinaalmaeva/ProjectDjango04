91d44b01603a212e7f4b36619c9778ab691a72da
irinaalmaeva <almaeva.irina@gmail.com>
2024-09-05 22:08:32 +0300

    add code

--- movie_project/films/views.py
+++ movie_project/films/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+# films/views.py
 
-# Create your views here.
+from django.shortcuts import render, redirect
+from .models import Film
+from .forms import FilmForm
+
+def add_film(request):
+    if request.method == 'POST':
+        form = FilmForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('film_list')
+    else:
+        form = FilmForm()
+    return render(request, 'films/film_form.html', {'form': form})
+
+def film_list(request):
+    films = Film.objects.all()
+    return render(request, 'films/film_list.html', {'films': films})
